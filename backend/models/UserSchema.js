6afb02a9de916e3e46093fb3dc4ff9a392ed25dc
ryangalaban <rscgalaban@up.edu.ph>
2024-05-07 23:12:25 +0800

    Created UserSchema.js chore: created UserSchema.js export User as a model

---
+++ backend/models/UserSchema.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+//Creating user schema to be used in MongoDB
+const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
