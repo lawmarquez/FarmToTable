4590568ba5b04cf1837cee4828029ed6026704b4
ryangalaban <rscgalaban@up.edu.ph>
2024-05-08 19:43:53 +0800

    Added landing page for Account and Home chore: simple landing page for Account and Home created

---
+++ frontend/users-auth/src/pages/Account.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './pages_css/Account.css'
+
+function Account() {
+  return (
+    <div>Account</div>
+  )
+}
+
+export default Account
---
+++ frontend/users-auth/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './pages_css/Home.css'
+
+function Home() {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
