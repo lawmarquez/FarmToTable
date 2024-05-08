b8328590a25ab213904f26df239a903a9cb1326d
ryangalaban <rscgalaban@up.edu.ph>
2024-05-08 17:58:13 +0800

    Corrected server port chore: removed duplicated app.listen in DB refactor: variable user to newUser fixed SECRET_KEY from SECREY_KEY

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const jwt = require('jsonwebtoken');
 
 //SCHEMA
 const User = require('./models/UserSchema');
-const SECREY_KEY = 'authentication'
+const SECRET_KEY = 'authentication'
 
 //App using Express
 const app = express();
@@ -20,36 +20,57 @@ mongoose.connect(dbURI, {
     useNewUrlParser: true, 
     useUnifiedTopology: true 
 })
-.then(() => app.listen(3001, () => {
+.then(() => {
     //Assigning port 3001 to the server (Accessing Database)
     app.listen(3001, () => {
         console.log('Server is running on port 3001 and connected to MongoDB');
     });
-}))
+})
 .catch((err) => {
     console.log('Unable to connect to MongoDB',err)
 });
 
 //Middleware (connecting code to database)
+app.use(bodyParser.json());
+app.use(cors());
 
 //Routes
 //Registration
 //POST Registration (sending requestto register)
-app.post('/register', async (req, res) => {
+app.post("/register", async(req, res)=>{
     try{
-        const { email, username, password } = req.body;
+        const {email, username, password} = req.body;
         //Password hashing for added security (10 as key rotation, normally 12 or 13)
         const hashedPassword = await bcrypt.hash(password, 10);
-        const user = new User({ email, username, password: hashedPassword });
-        await user.save();
-        res.status(201).send({message:'User has beenreated'});
-    }catch(err){
-        res.status(500).send({error: 'Error creating user'});
+        const newUser = new User({
+            email,
+            username,
+            password: hashedPassword
+        });
+        await newUser.save();
+        res.status(201).json({message: "User Registered"});
+    } catch(err){
+        res.status(500).json({error: 'Error registering new user'});
     }
+
 })
 
 //GET Registration (getting data from database)
 app.get("/register", async(req, res)=>{
+    try{
+        //Finding the user in the database
+        const users = await User.find();
+        res.status(201).json(users);
+    }catch(err){
+        res.status(500).json({error: 'Unable to get users'});
+    }
+})
+
+
+//Login
+//POST Login (getting data from database)
+
+app.post("/login", async(req, res)=>{
     try{
         const {username, password} = req.body;
         
