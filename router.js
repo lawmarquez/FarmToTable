61574a02cb62a6b3ee10a020b6e1f6cdfbda7253
jk-borja <jbborja@up.edu.ph>
2024-05-09 15:21:30 +0800

    chore: added update and delete routes

--- router.js
+++ router.js
@@ -1,4 +1,4 @@
-import {homepage, users, products, orderTransactions, saveUser, saveProduct, saveOrderTransaction, updateProductQty, updateCart, updateUser, updateOrderTransaction } from './controller.js'
+import { homepage, users, products, orderTransactions, saveUser, saveProduct, saveOrderTransaction, updateProductQty, updateCart, updateUser, updateOrderTransaction, deleteProduct, deleteUser, deleteCartProduct } from './controller.js'
 
 const router = (app) =>{
     app.get('/', homepage);
@@ -10,5 +10,14 @@ const router = (app) =>{
     app.post('/save-user', saveUser);
     app.post('/save-product', saveProduct);
     app.post('/save-order-transaction', saveOrderTransaction);
+
+    app.post('/update-productqty', updateProductQty);
+    app.post('/update-cart', updateCart);
+    app.post('/update-user', updateUser);
+    app.post('/update-ordertransaction', updateOrderTransaction);
+
+    app.post('/delete-product', deleteProduct);
+    app.post('/delete-user', deleteUser);
+    app.post('/delete-cartproduct', deleteCartProduct);
 }
 export default router;
