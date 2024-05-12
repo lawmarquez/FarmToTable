3c1a6a66182d0ad9941252d1c0a7e25dd2e97838
ryangalaban <rscgalaban@up.edu.ph>
2024-05-12 21:54:22 +0800

    Handle changes depending on the user ui: text “Admin Account” is displayed on NavBar if logged in user is “admin” clickable “Account” is still displayed if logged in user is “user” chore: added logic has been added to segregate user from admin console log has been added to  determine if user (any) has been logged in isAdmin has been added as a parameter docs: added docs to logic

--- frontend/users-auth/src/components/Navbar.js
+++ frontend/users-auth/src/components/Navbar.js
@@ -1,38 +1,48 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-
 import "./Navbar.css";
 
-function Navbar(){
-    //Check if local session (token) is available
+function Navbar({ isAdmin }) {
+    // Check if local session (token) is available
     const isUserSignedIn = !!localStorage.getItem("token");
+    console.log("isUserSignedIn:", isUserSignedIn); // Add this line to check the value
     const navigate = useNavigate();
 
-    //Handles Sign Out function that will void created token from Sign In
+    // Handles Sign Out function that will void created token from Sign In
     const handleSignOut = () => {
         localStorage.removeItem("token");
         navigate("/");
-    }
+    };
 
-    return(
+    return (
         <nav className="NavigationBar">
             <Link to="/">Home</Link>
             <ul>
-                {isUserSignedIn ? (
+                {/* renderint only the "Account" link if the user is signed in */}
+                {isUserSignedIn && (
                     <>
-                    <Link to='/account'><li>Account</li></Link>
-                    {/**Action that catches when user click the Sign out from NavBar */}
-                    <li><button onClick={handleSignOut}>Sign Out</button></li>
+                        {isAdmin ? (
+                            <li>Admin Account</li>
+                        ) : (
+                            <li>
+                                <Link to='/account'>Account</Link>
+                            </li>
+                        )}
+                        <li>
+                            <button onClick={handleSignOut}>Sign Out</button>
+                        </li>
                     </>
-                ) : (
+                )}
+                {/* showing the login and signup links if the user is not signed in */}
+                {!isUserSignedIn && (
                     <>
-                    <Link to='/login'><li>Login</li></Link>
-                    <Link to='/signup'><li>Signup</li></Link>
+                        <Link to='/login'><li>Login</li></Link>
+                        <Link to='/signup'><li>Signup</li></Link>
                     </>
                 )}
             </ul>
         </nav>
-    )
+    );
 }
 
 export default Navbar;
