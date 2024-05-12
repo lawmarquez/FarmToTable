f9e72623689d11a1a2a8607b7e3343cc9c32370a
ryangalaban <rscgalaban@up.edu.ph>
2024-05-12 21:47:42 +0800

    Added Redirection and changes in logic Chore: removed unnecessary imports and do some substitutions Added useState to determine where the user is signed in and is a user or an admin Added input for the NavBar Added logic if the user is signed in and  whether the user is an admin or regular user Added redirection to respective account page depending on the user type Added redirection if user is not logged in to home docs: added comments for logic

--- frontend/users-auth/src/App.js
+++ frontend/users-auth/src/App.js
@@ -1,25 +1,51 @@
-import { Routes, Route } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import SignUp from './pages/SignUp'
-import Account from './pages/Account'
-
-import './App.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import Account from './pages/Account';
+import AdminAccount from './pages/AdminAccount';
 
 function App() {
-  //Checking if user is signed in or not by checking session token
-  const isUserSignedIn = !!localStorage.getItem('token')
+  // Checking if user is signed in or not by checking session token
+  const [isUserSignedIn, setIsUserSignedIn] = useState(!!localStorage.getItem('token'));
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  {/**Check for isAdminProp to check is user is signed in for later checks*/}
+  const handleLoginSuccess = (isAdminProp) => {
+    setIsAdmin(isAdminProp);
+    setIsUserSignedIn(true);
+  };
+
+  {/**Handles logging out */}
+  const handleSignOut = () => {
+    localStorage.removeItem('token');
+    setIsUserSignedIn(false);
+    setIsAdmin(false);
+  };
 
   return (
     <div className="App">
-      <Navbar />
+      <Navbar isAdmin={isAdmin} isUserSignedIn={isUserSignedIn} handleSignOut={handleSignOut} />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
+        <Route path='/login' element={<Login onLoginSuccess={handleLoginSuccess} />} />
         <Route path='/signup' element={<SignUp />} />
-        {isUserSignedIn && <Route path='/account' element={<Account />} />}
+        {/* Only render the following routes if the user is signed in */}
+        {isUserSignedIn ? (
+          <>
+            {isAdmin ? (
+              <Route path='/admin-account' element={<AdminAccount />} />
+            ) : (
+              <Route path='/account' element={<Account />} />
+            )}
+          </>
+        ) : (
+          // Redirect to the home page if the user is not signed in (maybe insert to error page later on)
+          <Route path='/*' element={<Navigate to='/' />} />
+        )}
       </Routes>
     </div>
   );
