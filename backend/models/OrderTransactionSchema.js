1fa1fe2588feb5a0185db6596cd1e1e6eaa348fe
jk-borja <jbborja@up.edu.ph>
2024-05-18 02:57:55 +0800

    chore: created product and order transaction schema

---
+++ backend/models/OrderTransactionSchema.js
@@ -0,0 +1,36 @@
+import { Schema, model } from 'mongoose';
+
+//Creating OrderTransaction schema to be used in MongoDB
+const OrderTransactionSchema = new Schema({
+    tid: {
+        type: String,
+        required: true
+    },
+    pid: {
+        type: String,
+    },
+    oqty: {
+        type: Number,
+        required: true
+    },
+    ostatus: {
+        type: Number,   // 0 Pending, 1 Completed, 2 Cancelled
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true
+    }, 
+    time: {
+        type: Time,
+        required: true
+    }
+});
+
+const OrderTransaction = model('OrderTransaction', OrderTransactionSchema);
+
+export default OrderTransaction;
