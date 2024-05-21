85ad8368b222d64fe383ba64e5a74d1df19eaa50
lawmarquez <lumarquez@up.edu.ph>
2024-05-22 05:26:51 +0800

    chore: button css

--- frontend/users-auth/src/components/Navbar.js
+++ frontend/users-auth/src/components/Navbar.js
@@ -3,10 +3,17 @@ import { Link, useNavigate } from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar({ isUserSignedIn, isAdmin, handleSignOut }) {
+    const navigate = useNavigate();
+
+    const handleNavigation = (path) => {
+        navigate(path);
+    };
+
     return (
         <nav className="NavigationBar">
             <h1 className="headerTitle">Farm To Table</h1>
-            <Link to="/">Home</Link>
+            {/* <Link to="/">Home</Link> */}
+            <button onClick={() => handleNavigation("/")}>Home</button>
             <ul>
                 {/**Check if the user is Signed In using isUserSigned comning from the App.js */}
                 {isUserSignedIn ? (
@@ -14,16 +21,19 @@ function Navbar({ isUserSignedIn, isAdmin, handleSignOut }) {
                         {/** Check if the user logging in is an "admin" to change NavBar accordingly*/}
                         {isAdmin ? (
                             <li>
-                                <Link to="/admin-account">Admin Account</Link>
+                                {/* <Link to="/admin-account">Admin Account</Link> */}
+                                <button onClick={() => handleNavigation("/admin-account")}>Admin Account</button>
                             </li>
                         ) : (
                             <>
                                 <li>
-                                    <Link to='/account'>Account</Link>
+                                    {/* <Link to='/account'>Account</Link> */}
+                                    <button onClick={() => handleNavigation("/account")}>Account</button>
 
                                 </li>
                                 <li>
-                                    <Link to='/shop'>Shop</Link>
+                                    {/* <Link to='/shop'>Shop</Link> */}
+                                    <button onClick={() => handleNavigation("/shop")}>Shop</button>
                                 </li>
                             </>
                         )}
@@ -34,10 +44,12 @@ function Navbar({ isUserSignedIn, isAdmin, handleSignOut }) {
                 ) : (
                     <>
                         <li>
-                            <Link to='/login'>Login</Link>
+                            {/* <Link to='/login'>Login</Link> */}
+                            <button onClick={() => handleNavigation("/login")}>Login</button>
                         </li>
                         <li>
-                            <Link to='/signup'>Signup</Link>
+                            {/* <Link to='/signup'>Signup</Link> */}
+                            <button onClick={() => handleNavigation("/signup")}>Signup</button>
                         </li>
                     </>
                 )}
