d02fde1042e852f04da44a816db1c25de096f9da
jk-borja <jbborja@up.edu.ph>
2024-05-24 02:02:10 +0800

    chore: edited order fulfillment

--- frontend/users-auth/src/pages/admin_pages/OrderFulfillment.js
+++ frontend/users-auth/src/pages/admin_pages/OrderFulfillment.js
@@ -58,33 +58,44 @@ function OrderFulfillment() {
         }
     }
 
-    const status = (ostatus) => {
+    const product = productId => {
+        const prod = products.map((product) => (product.pid===productId? product.pname : ""))
+        return prod
+    }
+
+    const status = ostatus => {
         switch(ostatus){
-            case 0:
-                return "Pending"
             case 1:
                 return "Complete"
             case 2:
                 return "Cancelled"
+            default:
+                return "Pending"
         }
     } 
 
     return(
-        <div className="orderTransactions"> Order Fulfillment
-            {orderTransactions.map(orderTransaction => (
-                <div className="orderTransaction">
-                    <ul>
-                        <li>Order quantity: {orderTransaction.oqty}</li>
-                        <li>Order status: {status(orderTransaction.ostatus)}</li>
-                        <li>User email: {orderTransaction.email}</li>
-                        <li>Date of transaction: {orderTransaction.date}</li>
-                        <li>Time of transaction: {orderTransaction.time}</li>
-                        {orderTransaction.ostatus===0? <button className="completeOrder" onClick={() => completeOrderTransaction(orderTransaction.tid)}>Complete</button> : <></>}
-                    </ul>
-                    <br/>
-                </div>   
-            ))}
-        </div>
+        <>
+            <div className="orderFulfillment"> 
+            <h1>Order Fulfillment</h1>
+                {orderTransactions.map((orderTransaction, index) => (
+                    <div className="orderTransaction">
+                        <ul>
+                            Order #{index+1}
+                            <li>Order quantity: {orderTransaction.oqty}</li>
+                            <li>Product: {product(orderTransaction.pid)}</li>
+                            <li>Order status: {status(orderTransaction.ostatus)}</li>
+                            <li>User email: {orderTransaction.email}</li>
+                            <li>Date of transaction: {orderTransaction.date}</li>
+                            <li>Time of transaction: {orderTransaction.time}</li>
+                            {orderTransaction.ostatus===0? <button className="completeOrder" onClick={() => completeOrderTransaction(orderTransaction.tid)}>Complete</button> : <></>}
+                        </ul>
+                        <br/>
+                    </div>   
+                ))}
+            </div>
+        </>
+        
     )
 }
 
